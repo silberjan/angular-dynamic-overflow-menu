c9825c9b91483614abc172a746cbc87f7b177836
Jan Silbersiepe <jan.silbersiepe@targomo.com>
2021-05-15 21:15:05 +0200

    remove promise hack

--- src/lib/dynamic-overlay/dynamic-overlay.service.ts
+++ src/lib/dynamic-overlay/dynamic-overlay.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { fromEvent, Observable } from 'rxjs'
+import { fromEvent, merge, Observable } from 'rxjs'
 import { map, shareReplay } from 'rxjs/operators'
 
 @Injectable({
@@ -9,16 +9,13 @@ export class DynamicOverlayService {
   windowWidth$: Observable<number>
   displayInHost$: Observable<(bp: number) => boolean>
   constructor() {
-    this.windowWidth$ = fromEvent(window, 'resize').pipe(
-      shareReplay(1),
-      map(() => window.innerWidth)
+    this.windowWidth$ = merge(fromEvent(window, 'DOMContentLoaded'), fromEvent(window, 'resize')).pipe(
+      map(() => window.innerWidth),
+      shareReplay(1)
     )
     this.displayInHost$ = this.windowWidth$.pipe(
       map((windowWidth) => (breakpoint: number) => windowWidth >= breakpoint),
       shareReplay(1)
     )
-
-    // Trigger an initial window resize event. This is kind of hacky but anngular core team does it so its allowed --> https://github.com/angular/components/blob/71955d2e194bfc5561f25daea16e68af266d6ff9/src/material/select/select.ts#L868
-    Promise.resolve().then(() => window.dispatchEvent(new Event('resize')))
   }
 }
